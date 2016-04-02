69f643392e675e42305da88e1e64bf9a43490e6f
johnbolia <johnpbcmail@gmail.com>
2016-04-02 15:00:02 +0100

    streamline broken into class. Major refactoring to remove unneeded name functions

--- add_points.py
+++ add_points.py
@@ -1,38 +1,60 @@
 import pythoncom
 import win32com.client
+import csv
 import math
 
 
 class PointAdder(object):
 
     def __init__(self):
-        self.catia = win32com.client.Dispatch("CATIA.Application")
-        self.doc = self.catia.ActiveDocument
-        self.part = self.doc.Part
-        self.hyfac = self.part.HybridShapeFactory
-        self.g_set_list = []
-        gname = self.add_points('NN')
-        self.make_curve(gname)
-        jname = self.join_lines(gname, 'j1')
-        cname = self.create_ccurve(jname, 'c1')
-        self.delete_all_but_name(gname, cname)
+        try:
+            self.catia = win32com.client.Dispatch("CATIA.Application")
+            self.doc = self.catia.ActiveDocument
+            self.part = self.doc.Part
+            self.hyfac = self.part.HybridShapeFactory
+            self.sel = self.doc.Selection
+        except AttributeError:
+            print('CATIA must be loaded and a part activated')
+        else:
+            self.prelim_gset_list = []
+            print("CATPart '" + str(self.part.Name) + "' selected")
+            # gname = self.add_points('NN')
+            # self.make_curve(gname)
+            # jname = self.join_lines(gname, 'j1')
+            # cname = self.create_ccurve(jname, 'c1')
+            # self.delete_all_but_name(gname, cname)
 
-    def add_points(self, g_set_name):
-        self.part = self.catia.ActiveDocument.Part
-        self.hyfac = self.part.HybridShapeFactory
+    def add_file_locations(self):
+        self.data_folder = ('C:\\PME_Mirror\\GM_IndyCar\\Vehicle_Data\\Aero\\CFD\\RESULTS\\PHASE09-RC_DEV\\' +
+                            'GMICS-P09-C178 Data')
+        self.pv_folder = 'C:\\Program Files\\ParaView 5.0.0\\bin'
+        self.dat_file_folder = 'C:\\Users\\Johns Lenovo\\Documents\\GitHub\\Streamlines'
+        self.dat_file = 'group_01_1.dat'
+
+    def test(self):
+        sline_hbody = self.part.HybridBodies.Add()
+        sline_hbody_ref = self.part.CreateReferenceFromObject(sline_hbody)
+        self.part.HybridShapeFactory.ChangeFeatureName(sline_hbody_ref, "Streamlines")
+        seed_pt = self.part.HybridBodies.Item('Geometrical Set.1').HybridShapes.Item('Point.181')
+        sline = self.Streamline(self, seed_pt)
+        sline.open_dat_file('group_01_1.dat')
+        sline.create_lines()
+        sline.create_join()
+        cc = sline.create_ccurve()
+        sline_hbody.AppendHybridShape(cc)
+        self.sel.Clear()
+        self.sel.Add(sline.pre_hbody)
+        self.sel.Delete()
+
+    def add_points(self):
         hbody = self.part.HybridBodies.Add()
-        referencebody = self.part.CreateReferenceFromObject(hbody)
-        if self.check_for_hbody(g_set_name):
-            g_set_name = str(g_set_name) + "_1"
-        self.hyfac.ChangeFeatureName(referencebody, g_set_name)
         p = math.pi/180
         for i in range(180):
             point = self.hyfac.AddNewPointCoord(i, 100*math.sin(i*p), 100*math.cos(i*p))
             point.Name = 'Point_' + str(i+1)
             hbody.AppendHybridShape(point)
         self.part.update()
-        print(g_set_name)
-        return g_set_name
+        self.prelim_gset_list.append(hbody)
 
     def make_curve(self, g_set_name):
         hbody = self.part.HybridBodies.Item(g_set_name)
@@ -130,33 +152,85 @@ class PointAdder(object):
         else:
             return True
 
+    class Streamline(object):
 
+        def __init__(self, parent, seed_pt_obj):
+            self.part = parent.part
+            self.hyfac = parent.hyfac
+            self.seed_pt_obj = seed_pt_obj
+            self.seed_name = seed_pt_obj.Name
+            self.pre_hbody = self.part.HybridBodies.Add()
+            self.final_hbody = None
+            self.ref_pt_list = []
+            self.ref_line_list = []
+            
+            self.join_ref = None
 
+        def open_dat_file(self, dat_file):
+            with open(dat_file, 'r') as f:
+                dat_reader = csv.reader(f, delimiter=' ')
+                for row in dat_reader:
+                    self.add_point(row[0], row[1:])
+            self.part.update()
 
+        def add_point(self, name, coord_tuple):
+            point = self.hyfac.AddNewPointCoord(coord_tuple[0], coord_tuple[1], coord_tuple[2])
+            point.Name = name
+            self.pre_hbody.AppendHybridShape(point)
+            self.ref_pt_list.append(self.part.CreateReferenceFromObject(point))
 
+        def create_lines(self):
+            prev_ref = None
+            for ref in self.ref_pt_list:
+                if prev_ref:
+                    line = self.hyfac.AddNewLinePtPt(prev_ref, ref)
+                    self.ref_line_list.append(self.part.CreateReferenceFromObject(line))
+                    self.pre_hbody.AppendHybridShape(line)
+                prev_ref = ref
+            self.part.update()
 
+        def create_join(self):
+            join_created = 0
+            for line in self.ref_line_list:
+                if join_created == 0:
+                    line1 = line
+                    join_created = 1
+                elif join_created == 1:
+                    join = self.hyfac.AddNewJoin(line1, line)
+                    join_created = 2
+                else:
+                    join.AddElement(line)
+            set_join_params(join)
+            self.pre_hbody.AppendHybridShape(join)
+            self.join_ref = self.part.CreateReferenceFromObject(join)
+            self.part.update()
 
-
-
-    def set_join_params(self, join):
-        join.SetConnex(1)
-        join.SetManifold(1)
-        join.SetSimplify(0)
-        join.SetSuppressMode(0)
-        join.SetDeviation(0.001)
-        join.SetAngularToleranceMode(0)
-        join.SetAngularTolerance(0.5)
-        join.SetFederationPropagation(0)
-
-
+        def create_ccurve(self):
+            cc = self.hyfac.AddNewCurveSmooth(self.join_ref)
+            cc.SetTangencyThreshold(0.5)
+            cc.CurvatureThresholdActivity = False
+            cc.MaximumDeviationActivity = True
+            cc.SetMaximumDeviation(0.1)
+            cc.TopologySimplificationActivity = True
+            cc.CorrectionMode = 3
+            # self.final_hbody = self.part.HybridBodies.Add()
+            self.pre_hbody.AppendHybridShape(cc)
+            self.part.update()
+            ref2 = self.part.CreateReferenceFromObject(cc)
+            cc2 = self.hyfac.AddNewCurveDatum(ref2)
+            cc2.Name = 'Streamline_' + str(self.seed_name)
+            # hbody.AppendHybridShape(cc2)
+            # self.part.update()
+            self.hyfac.DeleteObjectForDatum(ref2)
+            return cc2
 
     def ref_junk_code(self):
         part = self.catia.ActiveDocument.Part
-        myHBody = part.HybridBodies.Add()
-        referencebody = part.CreateReferenceFromObject(myHBody)
+        hbody = part.HybridBodies.Add()
+        referencebody = part.CreateReferenceFromObject(hbody)
         part.HybridShapeFactory.ChangeFeatureName(referencebody, "New Name")
         point = part.HybridShapeFactory.AddNewPointCoord(10, 20, 30)
-        myHBody.AppendHybridShape(point)
+        hbody.AppendHybridShape(point)
         part.update()
         nn = part.HybridBodies.Item("New Name")
         np = nn.HybridShapes.Item("Point.1")
@@ -164,5 +238,20 @@ class PointAdder(object):
         nn.HybridShapes.Item(1).Name = 'licky'
         part.Parameters.Item("Part1\\New Name\\licky\\X").Value = 100
         number_of_items = nn.HybridShapes.Count
+        referencebody = self.part.CreateReferenceFromObject(hbody)
+        g_set_name = 'licky'
+        if self.check_for_hbody(g_set_name):
+            g_set_name = str(g_set_name) + "_1"
+        self.hyfac.ChangeFeatureName(referencebody, g_set_name)
 
+
+def set_join_params(join):
+    join.SetConnex(1)
+    join.SetManifold(1)
+    join.SetSimplify(0)
+    join.SetSuppressMode(0)
+    join.SetDeviation(0.001)
+    join.SetAngularToleranceMode(0)
+    join.SetAngularTolerance(0.5)
+    join.SetFederationPropagation(0)
 
