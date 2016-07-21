1ee1867b6e1988363639ed5fcef41d5494efe95e
johnbolia <johnpbcmail@gmail.com>
2016-07-21 10:21:13 +0100

    Update Checking functionality of sourcetree program

--- add_points.py
+++ add_points.py
@@ -4,6 +4,9 @@ import csv
 import subprocess
 import pdb
 import time
+import numpy as np
+import h5py
+from lxml import etree as ET
 
 
 class PointAdder(object):
@@ -23,8 +26,9 @@ class PointAdder(object):
             self.add_file_locations()
 
     def add_file_locations(self):
+        # self.data_folder = ''
         self.data_folder = ('C:\\PME_Mirror\\GM_IndyCar\\Vehicle_Data\\Aero\\CFD\\RESULTS\\PHASE09-RC_DEV\\' +
-                            'GMICS-P09-C178 Data')
+                            'GMICS-P09-C184\\')
         self.pv_folder = 'C:\\Program Files\\ParaView 5.0.0\\bin'
         self.dat_file_folder = 'C:\\Users\\Johns Lenovo\\Documents\\GitHub\\Streamlines'
         self.dat_file = 'group_01_1.dat'
@@ -33,19 +37,20 @@ class PointAdder(object):
         strln_hbody = self.create_hbody('Streamlines')
         if not dat_file:
             for strln_pt in self.strln_list:
-                strln = self.Streamline(self, seed_pt_obj=strln_pt.ref, max_time=max_time)
+                strln = self.Streamline(self, seed_pt_obj=strln_pt, max_time=max_time)
                 strln.open_dat_file()
-                self.__draw_strln__(strln, strln_hbody)
+                self._draw_strln(strln, strln_hbody)
         else:
             strln = self.Streamline(self, max_time=max_time)
             strln.open_dat_file(dat_file)
-            self.__draw_strln__(strln, strln_hbody)
+            self._draw_strln(strln, strln_hbody)
 
-    def __draw_strln__(self, strln, hbody):
+    def _draw_strln(self, strln, hbody):
         strln.create_lines()
         strln.create_join()
         cc = strln.create_ccurve()
-        hbody.AppendHybridShape(cc)
+        if cc is not None:
+            hbody.AppendHybridShape(cc)
         self.delete_with_selection(strln.pre_hbody)
 
     def delete_with_selection(self, obj):
@@ -68,8 +73,8 @@ class PointAdder(object):
         self.strln_list = []
         for i in range(self.sel.Count):
             if self.sel.Item(i+1).Type == 'HybridShapePointCoord':
-                hspoint = self.Point(self, self.sel.Item(i+1), direc=1)
-                hspoint2 = self.Point(self, self.sel.Item(i+1), direc=2)
+                hspoint = self.SeedPoint(self, self.sel.Item(i+1), direc=1)
+                hspoint2 = self.SeedPoint(self, self.sel.Item(i+1), direc=2)
                 self.strln_list.append(hspoint)
                 self.strln_list.append(hspoint2)
 
@@ -84,13 +89,22 @@ class PointAdder(object):
     def run_script(self):
         pvpy = '"' + self.pv_folder + '\\' + 'pvpython.exe"'
         pvscript = 'pvstreamextract2.py'
-        xmf = '"' + self.data_folder + '\\' + 'xdmf_avg0006000.xmf"'
+        xmf_inp = '"' + self.data_folder + 'xdmf_avg0006000_vel_only.xmf"'  # xdmf_avg0006000.xmf"'
+        geom_info = ' 26 30 121'  # -1 -1 0 1 0 1.5'
+        # self.trim_hd5(xmf_inp, geom_info)
+        # xmf = '"' + self.data_folder + '\\' + 'region.xmf"'
         pv_seed = 'pvstream_seed.dat'
-        geom_info = ' 26 30 121'
-        arg_string = pvpy + ' ' + pvscript + ' ' + xmf + ' ' + pv_seed + geom_info
+        arg_string = pvpy + ' ' + pvscript + ' ' + xmf_inp + ' ' + pv_seed + geom_info
+        print(arg_string)
         subprocess.call(arg_string, shell=True)
 
-    def __check_for_hbody__(self, hbody_name):
+    def trim_hd5(self, xmf_inp, allargs):
+        xmin = np.array([allargs[0], allargs[1], allargs[2]])
+        xmax = np.array([allargs[3], allargs[4], allargs[5]])
+        region_file = 'region.xmf'
+        write_region_xmf(xmf_inp, xmin, xmax, outputfile=region_file)
+
+    def _check_for_hbody(self, hbody_name):
         try:
             hbody = self.part.HybridBodies.Item(hbody_name)
         except pythoncom.com_error:
@@ -99,14 +113,14 @@ class PointAdder(object):
             return hbody
 
     def create_hbody(self, hbody_name, duplicate=False):
-        hbody_ref = self.__check_for_hbody__(hbody_name)
-        if not hbody_ref:
+        hbody = self._check_for_hbody(hbody_name)
+        if not hbody:
             hbody = self.part.HybridBodies.Add()
             # hbody_ref = self.part.CreateReferenceFromObject(hbody)
             hbody.Name = hbody_name
         return hbody
 
-    class Point(object):
+    class SeedPoint(object):
 
         def __init__(self, parent, hspoint, direc=1):
             self.name = hspoint.Value.Name
@@ -124,7 +138,6 @@ class PointAdder(object):
         def __init__(self, parent, seed_pt_obj=None, max_time=None):
             self.part = parent.part
             self.hyfac = parent.hyfac
-
             self.pre_hbody = self.part.HybridBodies.Add()
             self.ref_pt_list = []
             self.ref_line_list = []
@@ -134,17 +147,19 @@ class PointAdder(object):
             self.max_time = max_time
             self.seed_pt_obj = seed_pt_obj
             if seed_pt_obj:
-                self.seed_name = seed_pt_obj.Name
+                self.seed_name = seed_pt_obj.name + '_' + str(seed_pt_obj.direc)
 
         def open_dat_file(self, dat_file=None):
             if not dat_file:
-                dat_file = self.seed_name + '_1.dat'
+                dat_file = self.seed_name + '.dat'
             self.dat_file = dat_file
             with open(self.dat_file, 'r') as f:
                 dat_reader = csv.reader(f, delimiter=' ')
                 for row in dat_reader:
                     if float(row[0]) > self.max_time:
                         break
+                    elif -float(row[0]) > self.max_time:
+                        continue
                     self.add_point(row[0], row[1:])
             self.part.update()
 
@@ -176,12 +191,16 @@ class PointAdder(object):
                     join_created = 2
                 else:
                     join.AddElement(line)
+            if join_created < 2:
+                return
             set_join_params(join)
             self.pre_hbody.AppendHybridShape(join)
             self.join_ref = self.part.CreateReferenceFromObject(join)
             self.part.update()
 
         def create_ccurve(self):
+            if self.join_ref is None:
+                return
             created = False
             cc = self.hyfac.AddNewCurveSmooth(self.join_ref)
             base_dev = 0.1
@@ -210,7 +229,7 @@ class PointAdder(object):
             ref2 = self.part.CreateReferenceFromObject(cc)
             cc2 = self.hyfac.AddNewCurveDatum(ref2)
             if not self.seed_name:
-                self.seed_name = self.dat_file[:-6]
+                self.seed_name = self.dat_file[:-3]
             cc2.Name = 'Streamline_' + str(self.seed_name)
             self.hyfac.DeleteObjectForDatum(ref2)
             return cc2
@@ -233,3 +252,89 @@ def set_join_params(join):
     join.SetAngularToleranceMode(0)
     join.SetAngularTolerance(0.5)
     join.SetFederationPropagation(0)
+
+
+def write_region_xmf(xmffile, min, max, outputfile='region.xmf'):
+    """Write out an xmf file describing a region of the domain.
+    Any grid block that is outside of the bounding box will
+    not be written to the new region xmf file.
+
+    Parameters
+    ----------
+    xmffile : str
+        Name of the input xmf file.
+    min : list
+        Bounding box minimums in the x-, y- and z-directions.
+    max : list
+        Bounding box maximums in the x-, y- and z-directions.
+    outputfile : str, optional
+        Output xmf file name.
+
+    """
+
+    tree = ET.parse(xmffile)
+    root = tree.getroot()
+    try:
+        domain = root.find('.//Domain[@Name="Raven_Grid"]')
+        volume_and_boundaries = root.findall('.//Domain[@Name="Raven_Grid"]/Grid')
+        volume = root.find('.//Domain[@Name="Raven_Grid"]/Grid[@Name="Raven_Grid"]')
+        grids = volume.findall('.//Grid')
+        converted = False
+    except AttributeError:
+        domain = root.find('.//Domain[@Name="Raven Grid"]')
+        volume_and_boundaries = root.findall('.//Domain[@Name="Raven Grid"]/Grid')
+        volume = root.find('.//Domain[@Name="Raven Grid"]/Grid[@Name="Volume"]')
+        grids = volume.findall('.//Grid')
+        converted = True
+    grid_files = {}
+    include_grid = {}
+
+    # Loop over all the grids in the xmf file.
+    for n, grid in enumerate(grids):
+        grid_name = grid.attrib['Name']
+        include_grid[grid_name] = 1
+        geometry = grid.find('.//Geometry/DataItem[@Name="Nodes"]')
+        # Get the HDF5 file name and the dataset for the grid
+        grid_file, dataset = geometry.text.split(':')[:2]
+        if grid_file not in grid_files.keys():
+            # If we haven't already opened this file, then we need to.
+            grid_files[grid_file] = h5py.File(grid_file, 'r')
+        # Read the xyz dataset and compute the range for this block.
+        xyz = grid_files[grid_file][dataset][:]
+        xyz_min = xyz.min(0)
+        xyz_max = xyz.max(0)
+        for i in xrange(3):
+            if xyz_min[i] > max[i] or xyz_max[i] < min[i]:
+                # If this grid is outside the bounding box
+                # we do not need to keep it.
+                include_grid[grid_name] = 0
+                break
+
+    for f in grid_files.values():
+        f.close()
+
+    # Remove the unnecessary grids from the xmf file.
+    if converted:
+        for grid in volume_and_boundaries:
+            subgrids = grid.findall('.//Grid')
+            for subgrid in subgrids:
+                if include_grid[subgrid.attrib['Name']] == 0:
+                    grid.remove(subgrid)
+    else:
+        for grid in volume:
+            if include_grid[grid.attrib['Name']] == 0:
+                volume.remove(grid)
+
+    for grid in volume_and_boundaries:
+        subgrids = grid.findall('.//Grid')
+        if len(subgrids) == 0:
+            domain.remove(grid)
+
+    # Write out the new xmf file.
+    tree.write(outputfile,
+               encoding='utf-8',
+               xml_declaration=True,
+               pretty_print=True)
+
+
+
